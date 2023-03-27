5aeb9025b610f66472bcff29365aa0f15438a198
kmuli <tonny,kithui@sibasi.ltd>
2023-03-28 02:41:55 +0300

    Hero page reload projects component every 30 seconds

--- frontend/src/components/projects/ProjectDetails.jsx
+++ frontend/src/components/projects/ProjectDetails.jsx
@@ -1,33 +1,76 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ProjectCard from './ProjectCard';
 import './ProjectDetails.css';
 
 const ProjectDetails = () => {
     const projects = [
         {
-            id:1,
-            name:'Task mgt',
-            progress:34,
-            team:['Tonny', 'Skeet', 'Dreil'],
-            deadline:'2 weeks'
+            id: 1,
+            name: 'Task mgt',
+            progress: 34,
+            title: 'Am the tide',
+            team: ['Tonny', 'Skeet', 'Dreil'],
+            deadline: '2 weeks'
         },
         {
-            id:2,
-            name:'Chat design app',
-            progress:76,
-            team:['Tonny', 'Skeet', 'Dreil'],
-            deadline:'3 weeks'
-        }
-      ]
-  return (
-    <div className='projects-details'>
+            id: 2,
+            name: 'Chat design app',
+            progress: 76,
+            title: 'Cant find the right words',
+            team: ['Tonny', 'Skeet', 'Dreil'],
+            deadline: '3 weeks'
+        },
         {
-            projects.map((project) => (
-                <ProjectCard />
-            ))
+            id: 4,
+            name: 'School mgt project',
+            progress: 24,
+            title: 'Manage school info for students',
+            team: ['Skeet', 'Dreil'],
+            deadline: '2 weeks'
+        },
+        {
+            id: 5,
+            name: 'This is some other heading',
+            progress: 96,
+            title: 'This is just some random project',
+            team: ['Tonny', 'Skeet'],
+            deadline: '2 weeks'
+        },
+        ,
+        {
+            id: 6,
+            name: 'Some interesting project in .net core',
+            progress: 100,
+            title: 'This is just some random project',
+            team: ['Tonny', 'Skeet'],
+            deadline: '2 weeks'
         }
-    </div>
-  )
+    ]
+    const [projectIndex, setProjectIndex] = useState(0);
+    const [displayedProjects, setDisplayedProjects] = useState([
+        projects[projectIndex],
+        projects[projectIndex + 1]
+    ]);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            const newIndex = (projectIndex + 2) % projects.length;
+            setProjectIndex(newIndex);
+            setDisplayedProjects([projects[newIndex], projects[newIndex + 1]]);
+        }, 30000); // 2 minutes in milliseconds
+        console.log('Current pojects', displayedProjects);
+        return () => clearInterval(intervalId);
+    }, [projectIndex, projects]);
+
+    return (
+        <div className='projects-details'>
+            {
+                displayedProjects.map((project) => (
+                    <ProjectCard />
+                ))
+            }
+        </div>
+    )
 }
 
 export default ProjectDetails
