371061e1207dcee0d4f84ddfbf530d5aff403726
kmuli <tonny,kithui@sibasi.ltd>
2023-07-10 06:14:44 +0300

    Adding the search functionality for users

--- frontend/src/components/administration/Administration.jsx
+++ frontend/src/components/administration/Administration.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import Button from '../button/Button';
@@ -9,18 +9,41 @@ import Sidebar from '../sidebar/Sidebar';
 
 
 const Administration = () => {
+
+    const data = useSelector(state => state.usersFetchReducer.users.data);
+    const [search, setSearch] = useState('');
+    const [filteredUsers, setFilteredUsers] = useState(data);
+
     const isAuthenticated = useSelector(state => state.authReducer.isAuthenticated);
-    const data = useSelector(state => state.usersFetchReducer.users.data)
+    let newUserData = data;
+
+    const handleInputChange = (event) => {
+        const { value } = event.target;
+        setSearch(value);
+        console.log('VALUE', value)
+        console.log('SEARCH', search)
+        const filteredUsers = data.filter((user) => {
+            const fullName = `${user.firstname} ${user.lastname}`;
+            return fullName.toLowerCase().includes(value.toLowerCase());
+        });
+
+        setFilteredUsers(filteredUsers);
+    }
+
+    console.log('FILTERED USERS ', filteredUsers);
+    // console.log('FILTERED TEXT', search)
 
     return (
         <div className='administration'>
             <div className="admin-page">
-                <Sidebar />
+                <Sidebar value={search} onChange={(e) => setFilteredUsers(e.target.value)} />
+                
                 <div className="employee-cards p-2">
-                    <SearchBar />
+                {/* <input type="text" name="" id="" value={search} onChange={handleInputChange} /> */}
+                    <SearchBar value={search} onchange={handleInputChange}/>
                     <div className='admin'>
                         {
-                            data.map((user) => (
+                            filteredUsers.map((user) => (
                                 <Card user={user} />
                             ))
                         }
--- frontend/src/components/administration/SearchBar.jsx
+++ frontend/src/components/administration/SearchBar.jsx
@@ -3,11 +3,11 @@ import './Admin.css';
 import { BsSearch } from "react-icons/bs"
 
 
-const SearchBar = () => {
+const SearchBar = ({ value, onchange }) => {
     return (
         <div className='search-bar'>
             <BsSearch className='search-bar-icon' />
-            <input type="text" name="" id="" placeholder='Search...' />
+            <input type="text" name="" id="" placeholder='Search...' value={value} onChange={onchange}/>
             <h2>Go</h2>
         </div>
     )
